17a1830c491ced697894acfa3099258e6baf2038
YX Hao <lifenjoiner@163.com>
2026-09-21 18:54:26 +0800

    Improve command line option `/g`
    `/G line,[column[,tabwidth]]`
        The Tab is expanded to columns. tabwidth: 0, compatible with previous.
    `/g line,[character[,type]]`
        The Tab counts 1. type: 0, characters; 1, bytes.
    
    Ensure it goes to the correct location, if there are Tabs. More practical.
    This option is more likely designed for being used by other apps, which
    won't know the actual Tab width. It requires that We used the same encoding.

--- src/Notepad4.h
+++ src/Notepad4.h
@@ -68,6 +68,7 @@ struct NP2PARAMS {
 	int		iInitialLexer;
 	Sci_Line		iInitialLine;
 	Sci_Position	iInitialColumn;
+	int				iInitialColumnOpt;
 	int		iSrcEncoding;
 	int		flagSetEncoding;
 	int		flagSetEOLMode;
